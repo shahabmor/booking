be363335aa1cbb34931c913e70f2f1196b7033dc
Shahab Moradi <shahab.m.90@gmail.com>
2022-12-15 18:37:26 +0330

    define residences app & models

---
+++ residences/models.py
@@ -0,0 +1,103 @@
+from django.db import models
+
+
+# Policy model
+class Policy(models.Model):
+    title = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField(null=True, blank=True, default=None)
+
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+
+# Facility model
+class Facility(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True, default=None)
+
+    is_valid = models.BooleanField(default=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+
+# Image album models
+class ImageAlbum(models.Model):
+    title = models.CharField(max_length=50, null=True, blank=True, default=None)
+    description = models.TextField(null=True, blank=True, default=None)
+
+    is_valid = models.BooleanField(default=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=50, null=True, blank=True, default=None)
+    description = models.TextField(null=True, blank=True, default=None)
+    avatar = models.ImageField(upload_to='residences/', null=True, blank=True)
+
+    album = models.ForeignKey(ImageAlbum, related_name='images', on_delete=models.CASCADE)
+
+    is_valid = models.BooleanField(default=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+
+# Unit model
+class Unit(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+    capacity = models.PositiveSmallIntegerField(default=1)
+    bedroom = models.PositiveSmallIntegerField(default=0)
+    bed = models.PositiveSmallIntegerField(default=1)
+    image_album = models.OneToOneField(ImageAlbum, related_name='unit', on_delete=models.CASCADE)
+
+    facilities = models.ManyToManyField(Facility, related_name='units', default=None, null=True)
+    policies = models.ManyToManyField(Policy, related_name='units', default=None, null=True)
+
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+
+# Location models
+class Country(models.Model):
+    title = models.CharField(max_length=100)
+
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+
+class City(models.Model):
+    title = models.CharField(max_length=100)
+
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    country = models.ForeignKey(Country, related_name='cities', on_delete=models.CASCADE)
+
+
+# Hotel model
+class Hotel(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    capacity = models.PositiveSmallIntegerField(default=None, null=True)
+    image_album = models.OneToOneField(ImageAlbum, related_name='hotel', on_delete=models.CASCADE)
+
+    units = models.ManyToManyField(Unit, related_name='hotels', default=None, null=True)
+    facilities = models.ManyToManyField(Facility, related_name='hotels', default=None, null=True)
+    policies = models.ManyToManyField(Policy, related_name='hotels', default=None, null=True)
+
+    city = models.ForeignKey(City, related_name='hotels', default=None, null=True, on_delete=models.CASCADE)
+
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+
+# Residence model
+class Residence(Unit):
+    city = models.ForeignKey(City, related_name='residences', default=None, null=True, on_delete=models.CASCADE)
